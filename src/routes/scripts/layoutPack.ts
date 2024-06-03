69ffaad84771a6126a555f509559dc3b7affc4e4
Satrio Adi Rukmono <r.satrioadi@gmail.com>
2024-06-03 19:09:13 +0200

    Format code

--- src/routes/scripts/layoutPack.ts
+++ src/routes/scripts/layoutPack.ts
@@ -2,22 +2,22 @@
 // TODO: remove above line
 
 import {
-	FACTOR,
-	LAYER_SIZE_MULTIPLIER,
-	layers,
-	PADDING_INNER_PACK,
-	PADDING_INTER_PACK,
-	PADDING_X_LAYER
-} from '$lib/constant';
-import * as d3 from 'd3';
+  FACTOR,
+  LAYER_SIZE_MULTIPLIER,
+  layers,
+  PADDING_INNER_PACK,
+  PADDING_INTER_PACK,
+  PADDING_X_LAYER,
+} from "$lib/constant";
+import * as d3 from "d3";
 
 export default function layoutPack(roots: any[]) {
 	const layerCountProto: any = layers.reduce(
 		(obj: { [key: string]: number }, item: string) => {
-			obj[item] = 0;
-			return obj;
+		obj[item] = 0;
+		return obj;
 		},
-		{ 'Unknown Layer': 0 }
+		{ "Unknown Layer": 0 },
 	);
 	// COMPUTE
 	roots.forEach((root) => {
@@ -29,7 +29,7 @@ export default function layoutPack(roots: any[]) {
 		root.sum((d: any) => d.count);
 
 		// determine the max size of the root based on its value
-		const size = Math.log10(root.value+1) * FACTOR; // make it non linear
+		const size = Math.log10(root.value + 1) * FACTOR; // make it non linear
 		pack.size([size, size]);
 
 		pack(root);
@@ -38,26 +38,26 @@ export default function layoutPack(roots: any[]) {
 		const layerCount: any = { ...layerCountProto };
 		let total = 0;
 		root.each((d: any) => {
-			const layer = d.data.layer;
-			layerCount[layer] += d.data.count;
-			total += d.data.count;
+		const layer = d.data.layer;
+		layerCount[layer] += d.data.count;
+		total += d.data.count;
 		});
 
 		const layerPercentage = Object.keys(layerCount).reduce((acc: any, key) => {
-			acc[key] = (layerCount[key] / total) * 100;
-			return acc;
+		acc[key] = (layerCount[key] / total) * 100;
+		return acc;
 		}, {});
 
 		// 3. find the dominants layer, and remove non dominant layer.
 		/*
-		step
-		1. sort the layerPercentage from highest to lowest
-		2. if the second highest is more than 1.5 times the highest, the it's considered as dominant
-		3. do this until non dominan is found, then truncate the rest
-		*/
+			step
+			1. sort the layerPercentage from highest to lowest
+			2. if the second highest is more than 1.5 times the highest, the it's considered as dominant
+			3. do this until non dominan is found, then truncate the rest
+			*/
 		// turn the layerPercentage into array
 		const layerPercentageArray = Object.keys(layerPercentage).map((key) => {
-			return { layer: key, percentage: layerPercentage[key] };
+		return { layer: key, percentage: layerPercentage[key] };
 		});
 		// sort the layerPercentageArray from highest to lowest
 		layerPercentageArray.sort((a, b) => b.percentage - a.percentage);
@@ -65,14 +65,14 @@ export default function layoutPack(roots: any[]) {
 		const dominantLayer = [layerPercentageArray[0]];
 		let layerIndexToCheck = 1;
 		while (layerIndexToCheck < layerPercentageArray.length) {
-			const currentLayer = layerPercentageArray[layerIndexToCheck];
-			const previousLayer = layerPercentageArray[layerIndexToCheck - 1];
-			if (currentLayer.percentage * 1.5 > previousLayer.percentage) {
-				dominantLayer.push(currentLayer);
-			} else {
-				break;
-			}
-			layerIndexToCheck++;
+		const currentLayer = layerPercentageArray[layerIndexToCheck];
+		const previousLayer = layerPercentageArray[layerIndexToCheck - 1];
+		if (currentLayer.percentage * 1.5 > previousLayer.percentage) {
+			dominantLayer.push(currentLayer);
+		} else {
+			break;
+		}
+		layerIndexToCheck++;
 		}
 
 		// attach the layerPercentage to the root
@@ -95,7 +95,9 @@ function doesCircleOverlap(circle1: Circle, circle2: Circle) {
 
 // find the x and lock the y for the circle to not overlap new x must be to the right of the circle1
 function findMoveNeededInXForCircle2(circle1: Circle, circle2: Circle) {
-	const targetDistanceInX = Math.sqrt((circle1.r + circle2.r) ** 2 - (circle1.y - circle2.y) ** 2);
+	const targetDistanceInX = Math.sqrt(
+		(circle1.r + circle2.r) ** 2 - (circle1.y - circle2.y) ** 2,
+	);
 	if (Number.isNaN(targetDistanceInX)) {
 		return 0;
 	}
@@ -113,7 +115,7 @@ function calculateRootsLocationBasedOnDominantLayer(roots: any[]) {
 	// height is the biggest pack size
 	const biggestRootRadius = roots.reduce((acc, root) => {
 		if (acc.value < root.value) {
-			return root;
+		return root;
 		}
 		return acc;
 	});
@@ -123,46 +125,45 @@ function calculateRootsLocationBasedOnDominantLayer(roots: any[]) {
 	roots.forEach((root) => {
 		const dominantLayer = root.data.dominantLayer;
 		/*
-	if dominant layer > 2 ,set Y to be -100
-	if dominant layer = 2, check the two layer. if they're side to side, calculate based on the ratio of those two layer. 
-	if not, set Y to be -100
-	if dominant layer = 1, set Y the center of that layer
-*/
+		if dominant layer > 2 ,set Y to be -100
+		if dominant layer = 2, check the two layer. if they're side to side, calculate based on the ratio of those two layer.
+		if not, set Y to be -100
+		if dominant layer = 1, set Y the center of that layer
+		*/
 		if (dominantLayer.length > 2) {
-			root.containerY = -100;
-			root.isCrossOver = true;
+		root.containerY = -100;
+		root.isCrossOver = true;
 		} else if (dominantLayer.length === 2) {
+		const layer1 = dominantLayer[0].layer;
+		const layer2 = dominantLayer[1].layer;
+		const layer1Index = layers.indexOf(layer1);
+		const layer2Index = layers.indexOf(layer2);
+		if (layer1Index + 1 === layer2Index || layer1Index - 1 === layer2Index) {
 			const layer1 = dominantLayer[0].layer;
-			const layer2 = dominantLayer[1].layer;
 			const layer1Index = layers.indexOf(layer1);
+			const layer1Location = (layer1Index + 0.5) * layerHeight;
+			const layer1Percentage = dominantLayer[0].percentage;
+
+			const layer2 = dominantLayer[1].layer;
 			const layer2Index = layers.indexOf(layer2);
-			if (layer1Index + 1 === layer2Index || layer1Index - 1 === layer2Index) {
-				const layer1 = dominantLayer[0].layer;
-				const layer1Index = layers.indexOf(layer1);
-				const layer1Location = (layer1Index + 0.5) * layerHeight;
-				const layer1Percentage = dominantLayer[0].percentage;
+			const layer2Location = (layer2Index + 0.5) * layerHeight;
+			const layer2Percentage = dominantLayer[1].percentage;
 
-				const layer2 = dominantLayer[1].layer;
-				const layer2Index = layers.indexOf(layer2);
-				const layer2Location = (layer2Index + 0.5) * layerHeight;
-				const layer2Percentage = dominantLayer[1].percentage;
-
-				// calculate based on ratio
-				const distanceAdded =
-					(layer2Location - layer1Location) *
-					(layer2Percentage / (layer1Percentage + layer2Percentage));
-				root.containerY = layer1Location + distanceAdded;
-			} else {
-				root.containerY = -100;
-				root.isCrossOver = true;
-			}
-		} else if (dominantLayer.length === 1) {
-			const layer = dominantLayer[0].layer;
-			const layerIndex = layers.indexOf(layer);
-			root.containerY = (layerIndex + 0.5) * layerHeight;
+			// calculate based on ratio
+			const distanceAdded = (layer2Location - layer1Location) *
+			(layer2Percentage / (layer1Percentage + layer2Percentage));
+			root.containerY = layer1Location + distanceAdded;
 		} else {
 			root.containerY = -100;
 			root.isCrossOver = true;
+		}
+		} else if (dominantLayer.length === 1) {
+		const layer = dominantLayer[0].layer;
+		const layerIndex = layers.indexOf(layer);
+		root.containerY = (layerIndex + 0.5) * layerHeight;
+		} else {
+		root.containerY = -100;
+		root.isCrossOver = true;
 		}
 		// the coordinate is from top left.
 		root.containerY -= root.r;
@@ -172,30 +173,31 @@ function calculateRootsLocationBasedOnDominantLayer(roots: any[]) {
 
 		// skip if crossover
 		if (root.isCrossOver) {
-			return;
+		return;
 		}
 
 		let isOverlap = false;
 		do {
-			isOverlap = false;
-			for (let i = 0; i < previousRoots.length; i++) {
-				const previousRoot = previousRoots[i];
-				const circle1 = {
-					x: previousRoot.containerX + previousRoot.r, // center of the circle
-					y: previousRoot.containerY + previousRoot.r, // center of the circle
-					r: previousRoot.r
-				};
-				const circle2 = {
-					x: root.containerX + root.r, // center of the circle
-					y: root.containerY + root.r, // center of the circle
-					r: root.r
-				};
-				if (doesCircleOverlap(circle1, circle2)) {
-					isOverlap = true;
-					root.containerX += findMoveNeededInXForCircle2(circle1, circle2) + PADDING_INTER_PACK;
-					break;
-				}
+		isOverlap = false;
+		for (let i = 0; i < previousRoots.length; i++) {
+			const previousRoot = previousRoots[i];
+			const circle1 = {
+			x: previousRoot.containerX + previousRoot.r, // center of the circle
+			y: previousRoot.containerY + previousRoot.r, // center of the circle
+			r: previousRoot.r,
+			};
+			const circle2 = {
+			x: root.containerX + root.r, // center of the circle
+			y: root.containerY + root.r, // center of the circle
+			r: root.r,
+			};
+			if (doesCircleOverlap(circle1, circle2)) {
+			isOverlap = true;
+			root.containerX += findMoveNeededInXForCircle2(circle1, circle2) +
+				PADDING_INTER_PACK;
+			break;
 			}
+		}
 		} while (isOverlap);
 		previousRoots.push(root);
 	});
--- src/routes/scripts/renderPack.ts
+++ src/routes/scripts/renderPack.ts
@@ -1,12 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // TODO: remove above line
-import { adjustHexColor } from '$lib';
-import { colorMap, LAYER_SIZE_MULTIPLIER, layers, PADDING_X_LAYER } from '$lib/constant';
-import * as d3 from 'd3';
+import { adjustHexColor } from "$lib";
+import {
+	colorMap,
+	LAYER_SIZE_MULTIPLIER,
+	layers,
+	PADDING_X_LAYER,
+} from "$lib/constant";
+import * as d3 from "d3";
 
 export function renderLayer(
 	canvas: d3.Selection<SVGGElement, unknown, null, undefined>,
-	roots: any[]
+	roots: any[],
 ) {
 	const biggestRootRadius = roots.reduce((acc, root) => {
 		if (acc.value < root.value) {
@@ -17,33 +22,36 @@ export function renderLayer(
 
 	const layerHeight = biggestRootRadius.r * LAYER_SIZE_MULTIPLIER;
 
-	const defs = canvas.append('svg:defs');
+	const defs = canvas.append("svg:defs");
 
 	Object.entries(colorMap).forEach(([key, value]) => {
 		const grad = defs
-			.append('svg:radialGradient')
-			.attr('gradientUnits', 'objectBoundingBox')
-			.attr('cx', '25%')
-			.attr('cy', '25%')
-			.attr('r', '100%')
-			.attr('id', 'grad' + key.split(' ').join(''));
+			.append("svg:radialGradient")
+			.attr("gradientUnits", "objectBoundingBox")
+			.attr("cx", "25%")
+			.attr("cy", "25%")
+			.attr("r", "100%")
+			.attr("id", "grad" + key.split(" ").join(""));
 
-		grad.append('stop').attr('offset', '0%').style('stop-color', 'white');
+		grad.append("stop").attr("offset", "0%").style("stop-color", "white");
 
-		grad.append('stop').attr('offset', '75%').style('stop-color', adjustHexColor(value, -0.1));
+		grad.append("stop").attr("offset", "75%").style(
+			"stop-color",
+			adjustHexColor(value, -0.1),
+		);
 	});
 
 	const grad = defs
-		.append('svg:radialGradient')
-		.attr('gradientUnits', 'objectBoundingBox')
-		.attr('cx', '25%')
-		.attr('cy', '25%')
-		.attr('r', '100%')
-		.attr('id', 'gradElse');
+		.append("svg:radialGradient")
+		.attr("gradientUnits", "objectBoundingBox")
+		.attr("cx", "25%")
+		.attr("cy", "25%")
+		.attr("r", "100%")
+		.attr("id", "gradElse");
 
-	grad.append('stop').attr('offset', '0%').style('stop-color', '#D9B299');
+	grad.append("stop").attr("offset", "0%").style("stop-color", "#D9B299");
 
-	grad.append('stop').attr('offset', '75%').style('stop-color', '#ecd8cc');
+	grad.append("stop").attr("offset", "75%").style("stop-color", "#ecd8cc");
 
 	// calculate max width
 	let maxWidth = 0;
@@ -56,45 +64,45 @@ export function renderLayer(
 	const layerWidth = maxWidth + PADDING_X_LAYER;
 	layers.forEach((layer, i) => {
 		canvas
-			.append('rect')
-			.attr('x', 0)
-			.attr('y', i * layerHeight)
-			.attr('width', layerWidth)
-			.attr('height', layerHeight)
-			.attr('fill', () => adjustHexColor(colorMap[layer] ?? '#cccccc', 0.7))
-			.attr('stroke', 'black');
+			.append("rect")
+			.attr("x", 0)
+			.attr("y", i * layerHeight)
+			.attr("width", layerWidth)
+			.attr("height", layerHeight)
+			.attr("fill", () => adjustHexColor(colorMap[layer] ?? "#cccccc", 0.7))
+			.attr("stroke", "black");
 
 		canvas
-			.append('text')
-			.attr('x', 5)
-			.attr('y', i * layerHeight + 15)
-			.attr('fill', 'black')
-			.attr('text-anchor', 'start')
-			.attr('font-size', '14px')
+			.append("text")
+			.attr("x", 5)
+			.attr("y", i * layerHeight + 15)
+			.attr("fill", "black")
+			.attr("text-anchor", "start")
+			.attr("font-size", "14px")
 			.text(layer);
 	});
 	canvas
-		.append('rect')
-		.attr('x', maxWidth + PADDING_X_LAYER)
-		.attr('y', 0)
-		.attr('width', layerHeight)
-		.attr('height', 4*layerHeight)
-		.attr('fill', adjustHexColor(colorMap['Unknown Layer'], 0.7))
-		.attr('stroke', 'black');
+		.append("rect")
+		.attr("x", maxWidth + PADDING_X_LAYER)
+		.attr("y", 0)
+		.attr("width", layerHeight)
+		.attr("height", 4 * layerHeight)
+		.attr("fill", adjustHexColor(colorMap["Unknown Layer"], 0.7))
+		.attr("stroke", "black");
 	canvas
-		.append('text')
-		.attr('x', maxWidth + PADDING_X_LAYER + 5)
-		.attr('y', 15)
-		.attr('fill', 'black')
-		.attr('text-anchor', 'start')
-		.attr('font-size', '14px')
-		.text('Not classified');
+		.append("text")
+		.attr("x", maxWidth + PADDING_X_LAYER + 5)
+		.attr("y", 15)
+		.attr("fill", "black")
+		.attr("text-anchor", "start")
+		.attr("font-size", "14px")
+		.text("Not classified");
 }
 export function renderPack(
 	canvas: d3.Selection<SVGGElement, unknown, null, undefined>,
 	roots: any[],
 	writeDetailHover: (detail: string) => void,
-	handleRootClick: (root: any) => void
+	handleRootClick: (root: any) => void,
 ) {
 	let maxWidth = 0;
 	roots.forEach((root) => {
@@ -110,21 +118,21 @@ export function renderPack(
 			return;
 		}
 		const containerElement = canvas
-			.append('g')
-			.attr('transform', `translate(${root.containerX},${root.containerY})`)
-			.attr('id', `root-${index}`);
+			.append("g")
+			.attr("transform", `translate(${root.containerX},${root.containerY})`)
+			.attr("id", `root-${index}`);
 
 		// add container drag
 		containerElement.call(
-			d3.drag<any, any>().on('drag', function (event) {
+			d3.drag<any, any>().on("drag", function (event) {
 				// if not cross over
 				if (!root.isCrossOver) {
 					const x = event.dx;
-					// constraint on y axis
 					const y = 0;
 
 					root.containerX += x;
 					root.containerY += y;
+
 					// limit left and right boundary
 					if (root.containerX < 0) {
 						root.containerX = 0;
@@ -138,50 +146,56 @@ export function renderPack(
 					root.containerX += x;
 					root.containerY += y;
 				}
-				d3.select(this).attr('transform', `translate(${root.containerX},${root.containerY})`);
-			})
+				d3.select(this).attr(
+					"transform",
+					`translate(${root.containerX},${root.containerY})`,
+				);
+			}),
 		);
 
 		root.each((d: any) => {
 			const node = containerElement
-				.append('g')
-				.attr('transform', `translate(${d.x},${d.y})`)
-				.attr('class', 'node');
+				.append("g")
+				.attr("transform", `translate(${d.x},${d.y})`)
+				.attr("class", "node");
 
 			node
-				.append('circle')
-				.attr('r', d.r)
-				.attr('fill', 'url(#grad' + (d.data?.layer?.split(' ')?.join('') ?? 'Else') + ')')
-				.attr('stroke', 'black');
+				.append("circle")
+				.attr("r", d.r)
+				.attr(
+					"fill",
+					"url(#grad" + (d.data?.layer?.split(" ")?.join("") ?? "Else") + ")",
+				)
+				.attr("stroke", "black");
 
-			node.on('mouseover', function () {
-				const hoverDetail = JSON.stringify(d.data, null, '\t');
+			node.on("mouseover", function () {
+				const hoverDetail = JSON.stringify(d.data, null, "\t");
 				writeDetailHover(hoverDetail);
 
-				d3.select(this).select('circle').attr('stroke-width', 2);
+				d3.select(this).select("circle").attr("stroke-width", 2);
 			});
 
-			node.on('mouseout', function () {
-				writeDetailHover('{}');
-				d3.select(this).select('circle').attr('stroke-width', 1);
+			node.on("mouseout", function () {
+				writeDetailHover("{}");
+				d3.select(this).select("circle").attr("stroke-width", 1);
 			});
 
-			node.on('click', function () {
+			node.on("click", function () {
 				handleRootClick(root);
 			});
 
-			node.on('contextmenu', (event, d) => {
+			node.on("contextmenu", (event, d) => {
 				event.preventDefault();
-				console.log('right click', d);
+				console.log("right click", d);
 			});
 			// only show text for package
-			if (d.data.kind === 'package') {
+			if (d.data.kind === "package") {
 				node
-					.append('text')
-					.attr('dy', -d.r - 4)
-					.attr('fill', 'black')
-					.attr('text-anchor', 'middle')
-					.attr('font-size', '12px')
+					.append("text")
+					.attr("dy", -d.r - 4)
+					.attr("fill", "black")
+					.attr("text-anchor", "middle")
+					.attr("font-size", "12px")
 					.text(d.data.id);
 			}
 		});
