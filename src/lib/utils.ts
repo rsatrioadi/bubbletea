de68c57362d3dc67eb36c1a5d2631ec8042515aa
Aditya Prawira Nugroho <apn725@gmail.com>
2024-07-08 12:39:07 +0700

    feat: Added json converter into csv

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { cubicOut } from "svelte/easing";
 import type { TransitionConfig } from "svelte/transition";
-import type { DSVRowArray } from "d3";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -14,6 +14,25 @@ type FlyAndScaleParams = {
 	start?: number;
 	duration?: number;
 };
+
+interface JSONData {
+	elements: {
+		nodes: {data: any}[], 
+		edges: {data: any}[]
+	};
+}
+
+
+enum NodeKinds {
+	Method = "method",
+	Enum = "enum",
+	Interface = "interface", // doesn't
+	Class = "class",
+	Constructor = "ctor",
+	Package = "package",
+	AbstractClass = "abstract class",
+	Script = "script"
+}
 
 export const flyAndScale = (
 	node: Element,
@@ -62,11 +81,68 @@ export const flyAndScale = (
 	};
 };
 
-export function convertJsonToCsvText(json: JSON): string{
+export function convertJsonToCsvText(jsonData: JSONData): string{
 	const columns = "package,class,layer,count\n";
-	const csvText = columns
-	// Filter nodes without layer
-	// Parse which one is method which one is class
+	let csvText = columns
+	const layerMap = new Map<string, Map<string, {[key: string]: number}>>(); // Count the layers
+	const nodes = jsonData.elements.nodes
+
+	nodes.forEach((node) => {
+		// Process only nodes with layer attribute (data.properties.layer)
+		if (node.data.properties.layer !== undefined) {
+			// If kind is method, ctor, script, then add it to the map
+			if (node.data.properties.kind === NodeKinds.Method || node.data.properties.kind === NodeKinds.Constructor || node.data.properties.kind === NodeKinds.Script) {
+				// Parse the id
+				const id = node.data.id as string;
+				const className = id.replace(`.${node.data.properties.simpleName}`, "")
+				const packageName = className.split(".")?.reduce((acc, val, idx, arr) => {
+					if (idx === arr.length - 1) {
+						return acc;
+					} 
+					return acc.concat(`.${val}`);
+				})
+
+				// Check if the package is already in the map
+				if (layerMap.has(packageName)) {
+					// Check if the class is already in the map
+					const packageMap = layerMap.get(packageName);
+					if (packageMap!.has(className)) {
+						const classMap = packageMap!.get(className);
+						classMap![node.data.properties.layer] === undefined ? classMap![node.data.properties.layer] = 1 : classMap![node.data.properties.layer] += 1;
+					} else {
+						const classMap = {
+							[node.data.properties.layer]: 1
+						}
+						packageMap!.set(className, classMap);
+					}
+				} else {
+					const packageMap = new Map<string, {[key: string]: number}>();
+					const classMap = {
+						[node.data.properties.layer]: 1
+					}
+					packageMap.set(className, classMap);
+					layerMap.set(packageName, packageMap);
+				}
+			}
+		}
+	})
+
 	// Count them and aggregate the data
+	layerMap.forEach((packageMap, packageName) => {
+		packageMap.forEach((classMap, className) => {
+			// Count total of each layer
+			const layerCount = Object.keys(classMap).reduce((acc, key) => {
+				acc += classMap[key];
+				return acc;
+			}, 0);
+
+			// Insert into csvText
+			Object.keys(classMap).forEach((layer) => {
+				const count = classMap[layer]/layerCount;
+				csvText = csvText.concat(`${packageName},${className},${layer},${count}\n`);
+			})
+		})
+	})
+
 	return csvText;
 }
