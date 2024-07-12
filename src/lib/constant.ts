8f5f1e016608d9e933aa25002a78656b9426e4a2
Satrio Adi Rukmono <r.satrioadi@gmail.com>
2024-07-12 16:30:08 +0200

    Update color

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -5,11 +5,11 @@ export const PADDING_INNER_PACK = 2;
 export const PADDING_INTER_PACK = 10; // must be more than 1 or it will infinite loop. careful.
 
 export const colorMap: { [key: string]: string } = {
-	'Presentation Layer': '#ee3239',
-	'Service Layer': '#fece00',
-	'Domain Layer': '#5eaa5f',
-	'Data Source Layer': '#6a6dba',
-	'Unknown Layer': '#101010'
+	'Presentation Layer': '#FF3232',
+	'Service Layer': '#FFDD32',
+	'Domain Layer': '#1E991E',
+	'Data Source Layer': '#3232FF',
+	'Unknown Layer': '#002E49'
 };
 
 // arbitrary layer height: 3 times the biggest root radius
